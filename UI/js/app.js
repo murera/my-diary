1063727c75e14515422da6a89d949269fa517cd5
amanidiope@gmail.com <amanidiope@gmail.com>
2019-10-25 11:58:01 +0200

    feature(user-settings): create settings page
    add option for user to change his profile picture
    add option for user to turn on and off notifications
    [Finishes #169361498]

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,24 +1,33 @@
 	
-/* program toggle button */
-let navClose = document.getElementById('js-navbar-close');
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+const navSlide =()=>{
+  const bars = document.querySelector('.bars');
+  const nav= document.querySelector('.nav-links');
+  const navLinks= document.querySelectorAll('.nav-links li');
+  
+  
+  bars.addEventListener('click', ()=>{
+      
+      nav.classList.toggle('nav-active');
+      
+      
+      navLinks.forEach((link, index) => {
+              if(link.style.animation){
+                  link.style.animation="";
+              } 
+              else{
+                  link.style.animation = `navLinkFade 0.5s ease forwards ${index/ 7 + 0.3}s`
+              }
+          }
+      );
 
+      
+      bars.classList.toggle('toggle');
+  
+  });
+  
+}
 
-navBarToggle.addEventListener("click", function() {
-mainNav.classList.toggle("active");
-navBarToggle.classList.toggle("hide");
-navClose.classList.toggle("show");
-
-});
-navClose.addEventListener("click", function() {
-mainNav.classList.toggle("active");
-navBarToggle.classList.toggle("hide");
-navClose.classList.toggle("show");
-
-});
-
- 
+navSlide();
 
 // Dialog Box
 const dialogbox = (message) => { // Get the modal
@@ -37,15 +46,23 @@ const closeDialog = () => {
   modal.style.display = 'none';
 };
 
-const admin = () =>{
-  let show__button = document.querySelector('.nav-trigger');
 
-  show__button.addEventListener('click',function(){
-         document.querySelector('.side-nav').classList.toggle("visible");
-        
-      
-     });
-};
+
+let userUpdate=document.querySelector('.update-message');
+let rejectsession= document.querySelectorAll('.update-info')
+rejectsession.forEach((btn)=>
+{
+ 
+   btn.addEventListener('click',()=>
+   {
+    userUpdate.style.display='block'
+    setTimeout(()=>{userUpdate.style.display='none'},1000)
+    
+
+   })
+})
+
+
 
 
 
