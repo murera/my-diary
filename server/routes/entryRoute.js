2b29fab6da147cc6804fbc78dec5de8f32088fe1
amanidiope@gmail.com <amanidiope@gmail.com>
2019-11-05 17:47:38 +0200

    feature(get entry): add get specific entry endpoint
    implement a method to fetch a specific entry from databse
    write unit tests
    [Finishes #169550169]

--- server/routes/entryRoute.js
+++ server/routes/entryRoute.js
@@ -2,13 +2,13 @@ import express from 'express';
 import EntryController from '../controllers/entryController';
 import isUserExist from '../middleware/isUserExist';
 import {
-  entryValidator,
+  entryValidator, permission,
 }
   from '../middleware/validator';
 
 const router = express.Router();
 const {
-  createEntry, getMyEntries,
+  createEntry, getMyEntries, getSpeciEntry,
 } = EntryController;
 
 router.post('/entries',
@@ -19,4 +19,8 @@ router.post('/entries',
 router.get('/entries',
   isUserExist,
   getMyEntries);
+  router.get('/entries/:entryId',
+  isUserExist,
+  permission,
+  getSpeciEntry);
 export default router;
