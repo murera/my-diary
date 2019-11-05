c1eb4ef52d32d2f6c0553013e9711a40ae635f2e
amanidiope@gmail.com <amanidiope@gmail.com>
2019-11-05 15:19:10 +0200

    chore(travis): write unit tests [Finshes #169549798]

--- server/test/Xusertest.js
+++ server/test/Xusertest.js
@@ -79,3 +79,118 @@ describe('User sign up ', () => {
     done();
   });
 });
+
+describe('User Login ', () => {
+  beforeEach('Create a user', (done) => {
+    chai.request(app)
+      .post('/api/v2/auth/signup')
+      .send(testData[4])
+      .end((error) => {
+        if (error) done(error);
+        done();
+      });
+  });
+  it('it should login a user', (done) => {
+    chai.request(app)
+      .post('/api/v2/auth/signin')
+      .send(testData[5])
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property('status').eql(200);
+        res.body.should.have.property('data');
+        res.body.data.should.be.a('object');
+        res.body.data.should.have.property('token');
+        res.body.data.should.have.property('id');
+        res.body.data.should.have.property('firstname').eql('amani');
+        res.body.data.should.have.property('lastname').eql('murera');
+        done();
+      });
+  });
+  it('it should not login user without email', (done) => {
+    chai.request(app)
+      .post('/api/v2/auth/signin')
+      .send(testData[6])
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('status').eql(400);
+        res.body.should.have.property('error');
+        done();
+      });
+  });
+  it('it should not login user without password', (done) => {
+    chai.request(app)
+      .post('/api/v2/auth/signin')
+      .send(testData[7])
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.have.property('status').eql(400);
+        res.body.should.have.property('error');
+        done();
+      });
+  });
+
+  // incorrect password
+  it('it should not login user with wrong password', (done) => {
+
+    chai.request(app)
+      .post('/api/v2/auth/signin')
+      .send(testData[8])
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.have.property('status').eql(401);
+        res.body.should.have.property('error');
+        res.body.should.have.property('error').eql('email or password is incorrect!');
+        done();
+      });
+  });
+
+
+  // user not found
+  it('it should not login user who does not have acount', (done) => {
+    chai.request(app)
+      .post('/api/v2/auth/signin')
+      .send(testData[9])
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.have.property('status').eql(401);
+        res.body.should.have.property('error');
+        res.body.should.have.property('error').eql('email or password is incorrect!');
+        done();
+      });
+  });
+});
+
+
+describe('POST api/v2/auth/signup when endpoint does not exist', () => {
+it('should return incorrect route', (done) => {
+chai.request(app)
+.post('/api/v2')
+.set('Accept', 'application/json')
+.end((err, res) => {
+  res.should.have.status(404);
+  res.body.should.be.an('object');
+  res.body.should.have.property('status').eql(404);
+  res.body.should.have.property('error');
+  res.body.should.have.property('error').eql('Incorrect route!');
+  done();
+});
+});
+});
+describe('POST api/v2/auth/signup when JSON format is incorrect', () => {
+it('should return incorrect JSON format', (done) => {
+chai.request(app)
+.post('/api/v2/auth/signup')
+.set('Accept', 'application/json')
+.set('Content-Type', 'application/json')
+.send('{"invalidJson"}')
+.end((err, res) => {
+  res.should.have.status(400);
+  res.body.should.be.an('object');
+  res.body.should.have.property('status').eql(400);
+  res.body.should.have.property('error');
+  res.body.should.have.property('error').eql('invalid JSON format!');
+  done();
+});
+});
+});
+
