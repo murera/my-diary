b1d77f2c38995fdd1870101a7ff86a74ed670ce1
amanidiope@gmail.com <amanidiope@gmail.com>
2019-11-05 14:33:50 +0200

    bug(entry): remove functionlity to check if entry title exist [Finishes #169549303]

--- server/controllers/entryController.js
+++ server/controllers/entryController.js
@@ -12,12 +12,6 @@ class EntryController {
         let { title, description } = req.body;
         let token = req.header('x-auth-token');
         const owner = grabEmployeeIdFromToken(token, res);
-        const findIfEntryexist = 'SELECT * FROM entries WHERE title = $1 and ownerid = $2';
-        const entryExist = await Database.execute(findIfEntryexist, [title, owner]);
-        if (entryExist[0]) {
-          return ResponseHandler.error(REQUEST_CONFLICT, 'The same entry exists', res);
-        }
-
         const createEntry = 'INSERT INTO entries (title, description, ownerId) VALUES ($1, $2, $3) RETURNING *';
         const entryCreated = await Database.execute(createEntry,
           [title, description, owner]);
@@ -26,6 +20,7 @@ class EntryController {
         return ResponseHandler.error(SERVER_ERROR, `Internal server error occured: ${e} `, res);
       }
     };
+
 }
 
 export default EntryController;
