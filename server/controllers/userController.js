de29cb10396af01bb9cda8e16daf62c3c24bd407
amanidiope@gmail.com <amanidiope@gmail.com>
2019-11-06 15:04:37 +0200

    chore(documentation): update api documentation [Finishes #169596595]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 import generateAuthToken from '../helpers/tokenEncoder';
-import encryptPassword from '../helpers/passwordEncryptor';
-import comparePassword from '../helpers/passwordMatcher';
+import encryptPassword from '../helpers/hashPassword';
+import comparePassword from '../helpers/comparePassword';
 import ResponseHandler from '../helpers/responseHandler';
 import Database from '../models/database';
 import {
